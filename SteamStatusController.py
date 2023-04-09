1f74812044ab0aa91166dfaef7f803ba96d496f2
Samed763 <101745582+Samed763@users.noreply.github.com>
2023-04-09 20:32:24 +0300

    Add files via upload

---
+++ SteamStatusController.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import telebot
+
+TOKEN = '5843401653:AAEAwufmtfWQ8b-yzBkBu8S3TNTc1EvcnGw'
+tb = telebot.TeleBot(TOKEN)
+user = tb.get_me()
+
+driver = webdriver.Chrome()
+driver.get("https://steamstat.us/")
+
+degisken_1=None
+
+while True:
+
+    Durum = driver.find_element(By.XPATH ,'//*[@id="csgo_community"]')
+
+    element_text = Durum.text
+    print('1.Sunucun Durumu:'+element_text)
+
+    if(degisken_1==None):
+        tb.send_message(-965323938, "----BOT BAŞLATILDI----")
+
+    if(degisken_1 != element_text):
+        tb.send_message(-965323938, "Sunucu Durumu:"+" "+element_text)
+        print("*------DURUMLAR DEĞİŞTİ------*")
+        time.sleep(5)
+        degisken_1=element_text
+
+    else:
+        time.sleep(5) 
+        print("*------------ ELSE ÇALIŞTI ------------*")
